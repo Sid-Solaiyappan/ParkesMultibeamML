b212cfe2f9ed76e042f727ff62fa2643f8c689d8
Sid S <68795668+Sid-Solaiyappan@users.noreply.github.com>
2023-08-07 14:40:11 -0700

    Update highF_injSignal.py

--- highF_injSignal.py
+++ highF_injSignal.py
@@ -1,4 +1,4 @@
-# import os
+import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -72,32 +72,3 @@ for inj_h5 in inj_signal:
             
     except:
         pass
-        
-#     fmin,fmax = float(header[-3].split(' ')[-1]),float(header[-2].split(' ')[-1])
-
-#     if fmin>1359.5-5 and fmin<1359.5+5 and fmax>1513.5-5 and fmax<1513.5+5:
-    
-#         for i in range(140):
-#             f_start = np.random.uniform(fmin,fmax-diff)
-#             f_stop = f_start+diff
-#             drift = np.random.uniform(-4,4)
-#             snr = np.random.uniform(10,40)
-#             ind = np.random.uniform(0.2*1954,0.8*1954)
-#             print(f"sample : {sample_num}\nf_start: {f_start}\nf_stop : {f_stop}\ndrift  : {drift}\nsnr    : {snr}\nind    : {ind}\n{inj_h5}")
-#             obs = bl.Waterfall(inj_h5,f_start=f_start,f_stop=f_stop)
-
-#             frame = stg.Frame(waterfall = obs)
-#     #         fig = plt.figure(figsize=(7, 4))
-#             signal = frame.add_signal(stg.constant_path(f_start=frame.get_frequency(ind),
-#                                                         drift_rate=drift*u.Hz/u.s),
-#                                       stg.constant_t_profile(level=frame.get_intensity(snr=snr)),
-#                                       stg.gaussian_f_profile(width=40*u.Hz),)
-#         #     frame.bl_plot()
-#             data = frame.get_data()
-#         #     plt.show()
-#             sample_num+=1
-#             img = plt.imshow(frame.data,aspect='auto',interpolation='none').make_image(renderer=None, unsampled=False)[0][:,:,:3]
-#             im = Image.fromarray(img)#.convert('L')
-#             im.save('highF_injSignal/img_{0:06}.png'.format(sample_num))
-# #             np.save("highF_injSignal/img_{0:06}.npy".format(sample_num),data,allow_pickle=True)
-            
